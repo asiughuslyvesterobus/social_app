f10c7e78e376c13502d868019c9220a44af48043
gift56 <efeasiughu@gmail.com>
2023-09-13 18:33:57 -0400

    updating ui

---
+++ client/layout/MainLayout.tsx
@@ -0,0 +1,27 @@
+import { ReactNode } from "react";
+// import { LeftSideBar, RightSidebar } from "@/components";
+
+type LayoutProps = {
+  children: ReactNode;
+  showRight: boolean;
+};
+
+const MainLayout = ({ children, showRight }: LayoutProps) => {
+  return (
+    <main className="lg:w-[1200px] m-auto overflow-hidden h-screen flex items-start justify-between flex-col lg:flex-row">
+      <div className="lg:flex-[1] w-full border-b lg:border-none">
+        "LeftSideBar "
+      </div>
+      <div className="lg:flex-[2] w-full h-screen overflow-y-auto scrollbar-hide">
+        {children}
+      </div>
+      {showRight === true && (
+        <div className="lg:flex-[1] hidden lg:flex">
+          "RightSidebar"
+        </div>
+      )}
+    </main>
+  );
+};
+
+export default MainLayout;
