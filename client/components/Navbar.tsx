05e6295667c66b8f631bc123140332e0284ae69f
gift56 <efeasiughu@gmail.com>
2023-09-13 19:17:17 -0400

    working on navbar

--- client/components/Navbar.tsx
+++ client/components/Navbar.tsx
@@ -1,16 +1,28 @@
 import Head from "next/head";
+import Image from "next/image";
 
 const Navbar = ({ title }: { title: String }) => {
   return (
     <>
       <Head>
-        <title>{`ChatSmart - ${title}`}</title>
+        <title>{`ConnectSmart - ${title}`}</title>
         <meta
           name="description"
           content="Connecting People, One Post at a Time"
         />
       </Head>
-      <nav className="w-full h-20 bg-white"></nav>
+      <nav className="w-full h-16 bg-white flex items-center justify-between gap-10">
+        <div className="flex items-center justify-start gap-3">
+          <Image
+            src="/img/logo.png"
+            alt="ConnectSmart Logo"
+            width={62}
+            height={48}
+          />
+        </div>
+        <div></div>
+        <div></div>
+      </nav>
     </>
   );
 };
