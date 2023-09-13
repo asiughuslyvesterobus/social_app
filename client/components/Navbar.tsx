653158413aab20ab72b624cee97293e23033d38b
gift56 <efeasiughu@gmail.com>
2023-09-13 18:49:37 -0400

    adding varibles

--- client/components/Navbar.tsx
+++ client/components/Navbar.tsx
@@ -9,6 +9,16 @@ const Navbar = ({ title }: { title: String }) => {
           name="description"
           content="Connecting People, One Post at a Time"
         />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin=""
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;500;600;700;800&display=swap"
+          rel="stylesheet"
+        ></link>
       </Head>
       <nav>Navbar</nav>
     </>
