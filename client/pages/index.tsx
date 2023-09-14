5e53c20a12311dbbb66532365a65ac64c563fd25
gift56 <efeasiughu@gmail.com>
2023-09-14 08:08:50 -0400

    working on main page

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -8,7 +8,7 @@ const Homepage = () => {
         <div className="lg:flex-[0.8] w-full hidden md:flex">
           <LeftSidebar />
         </div>
-        <div className="lg:flex-[2] w-full h-screen overflow-y-auto scrollbar-hide p-4">
+        <div className="lg:flex-[2] w-full h-screen overflow-y-auto scrollbar-hide p-4 flex flex-col items-start justify-start gap-4">
           <div></div>
         </div>
         <div className="lg:flex-[0.8] hidden lg:flex">
