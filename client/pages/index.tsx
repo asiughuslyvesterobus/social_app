b07b7402f7c41a65770b9050b8202d3bcc306350
gift56 <efeasiughu@gmail.com>
2023-09-14 08:03:30 -0400

    working on main page

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,11 +1,12 @@
+import { LeftSidebar, RightSidebar } from "@/components";
 import MainLayout from "@/layout/MainLayout";
 
 const Homepage = () => {
   return (
     <MainLayout>
       <section className="w-full h-screen overflow-hidden flex items-start justify-start">
-        <div className="lg:flex-[1] w-full border-b lg:border-none">
-          <LeftSideBar />
+        <div className="lg:flex-[1] w-full  hidden lg:flex">
+          <LeftSidebar />
         </div>
         <div className="lg:flex-[2] w-full h-screen overflow-y-auto scrollbar-hide">
           children
