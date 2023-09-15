a38438a59c639de0ae6e6d997dc443d1d40c1014
gift56 <efeasiughu@gmail.com>
2023-09-15 09:48:58 -0400

    done working on left side bar

--- client/components/RightSidebar.tsx
+++ client/components/RightSidebar.tsx
@@ -1,10 +1,20 @@
+import { FaUserCircle } from "react-icons/fa";
+
 const RightSidebar = () => {
+  const contacts = [
+    "Jane Cooper",
+    "Islam Ali",
+    "John Doe",
+    "Albert Flores",
+    "Jason Dodd",
+  ];
+
   return (
     <nav
       aria-label="sidebar"
       className="w-full h-screen overflow-y-auto overflow-x-hidden bg-white p-3 flex flex-col items-start justify-start"
     >
-      RightSidebar
+        
     </nav>
   );
 };
