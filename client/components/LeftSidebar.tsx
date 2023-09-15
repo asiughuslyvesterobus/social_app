e54c3d550d5d6dc7fa7196dd7eb02a709f23c411
gift56 <efeasiughu@gmail.com>
2023-09-15 08:31:31 -0400

    working on left side bar

--- client/components/LeftSidebar.tsx
+++ client/components/LeftSidebar.tsx
@@ -51,10 +51,28 @@ const LeftSidebar = () => {
       </ul>
 
       <div className="flex flex-col items-start w-full justify-start gap-3 pt-3">
-        <h4 className="text-lg font-semibold text-basegray tracking-tight">Your shortcuts</h4>
+        <h4 className="text-lg font-semibold text-basegray tracking-tight">
+          Your shortcuts
+        </h4>
         <ul className="flex flex-col items-start w-full justify-start gap-1">
           {[0, 1, 2, 3].map((item, i) => (
-            <li key={i} className=""></li>
+            <li key={i} className="w-full">
+              <Link
+                href="/"
+                className="flex items-center justify-start w-full gap-2 hover:bg-bodybg py-2 px-2 rounded transition-all"
+              >
+                <Image
+                  src="/img/shortcut_image.png"
+                  alt="shortcuts-images"
+                  width={30}
+                  height={30}
+                  className="rounded"
+                />
+                <span className="text-sm font-semibold text-black tracking-tight">
+                  UI / UX Designers & Developers
+                </span>
+              </Link>
+            </li>
           ))}
         </ul>
       </div>
