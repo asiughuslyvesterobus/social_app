bd732da85a23a04f4ef50de23755b87a33db7ade
gift56 <efeasiughu@gmail.com>
2023-09-15 08:42:36 -0400

    done working on left side bar

--- client/components/Footer.tsx
+++ client/components/Footer.tsx
@@ -6,17 +6,17 @@ const year = date.getFullYear();
 const Footer = () => {
   return (
     <footer className="block mt-4">
-      <ul className="flex flex-wrap gap-3">
+      <ul className="flex flex-wrap gap-2">
         {footerList.map((item) => (
           <li
             key={item}
-            className="text-basegray text-sm font-normal hover:underline cursor-pointer"
+            className="text-basegray text-sm font-normal hover:underline cursor-pointer flex items-center group"
           >
-            {item}
+            {item} <span className="w-1 inline-block h-1 bg-basegray rounded-full ml-1 group-last:hidden"></span>
           </li>
         ))}
       </ul>
-      <p className="text-basegray text-sm font-normal">{year} ConnectSmart</p>
+      <p className="text-basegray text-sm font-normal">&copy; {year} ConnectSmart</p>
     </footer>
   );
 };
