bbd1e3297718c4b53260c9c40ce15001899fd479
gift56 <efeasiughu@gmail.com>
2023-09-15 08:05:15 -0400

    working on left side bar

--- client/components/LeftSidebar.tsx
+++ client/components/LeftSidebar.tsx
@@ -1,5 +1,31 @@
+import Image from "next/image";
+import Link from "next/link";
+
 const LeftSidebar = () => {
-  return <nav aria-label="sidebar" className="w-full h-screen overflow-y-auto overflow-x-hidden bg-white p-3 flex flex-col items-start justify-start">LeftSidebar</nav>;
+  const user = null;
+  return (
+    <nav
+      aria-label="sidebar"
+      className="w-full h-screen overflow-y-auto overflow-x-hidden bg-white p-3 flex flex-col items-start justify-start"
+    >
+      {user ? (
+        "Loggin"
+      ) : (
+        <Link
+          href={`/profile/42`}
+          className="flex items-center justify-start w-full gap-2"
+        >
+          <Image
+            src="/img/avatar.png"
+            alt="user_profile"
+            width={30}
+            height={30}
+            className="rounded-full"
+          />
+        </Link>
+      )}
+    </nav>
+  );
 };
 
 export default LeftSidebar;
