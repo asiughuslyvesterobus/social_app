481f463a1c744ab073bb143c9bdd0899d82ef498
gift56 <efeasiughu@gmail.com>
2023-09-15 08:33:22 -0400

    working on left side bar

--- client/components/Footer.tsx
+++ client/components/Footer.tsx
@@ -1,5 +1,26 @@
+import { footerList } from "@/data";
+
+const date = new Date();
+const year = date.getFullYear();
+
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className="block">
+      <div className="flex flex-wrap gap-2">
+        {footerList.map((item) => (
+          <p
+            key={item}
+            className="text-gray-400 dark:text-gray-200 text-sm hover:underline cursor-pointer"
+          >
+            {item}
+          </p>
+        ))}
+      </div>
+      <p className="text-gray-400 dark:text-gray-200 text-sm mt-5">
+        {year} CS Social
+      </p>
+    </div>
+  );
 };
 
 export default Footer;
