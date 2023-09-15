c4b3687822c9191e529db85be5925596aebe5d3a
gift56 <efeasiughu@gmail.com>
2023-09-15 16:49:59 -0400

    working on storypage

--- client/components/StoryCard.tsx
+++ client/components/StoryCard.tsx
@@ -3,23 +3,27 @@ import Image from "next/image";
 
 const StoryCard = ({ profileImg, profileName, storyImage }: StoryProp) => {
   return (
-    <div className="w-full h-full rounded-md relative overflow-hidden group cursor-pointer">
-      <Image
-        src={storyImage}
-        alt="story"
-        width={100}
-        height={100}
-        className="w-full h-full group-hover:scale-[1.08] transition-all"
-      />
-      <div className="absolute w-full h-full bg-black/20 group-hover:bg-black/50 transition-all inset-0 flex items-start justify-between p-4 flex-col">
+    <div className="relative inline-block cursor-pointer w-[200px] mx-1 h-full">
+      <div className="w-full h-full rounded-md relative overflow-hidden group cursor-pointer">
         <Image
-          src={profileImg}
-          alt="user_profile"
-          width={35}
-          height={35}
-          className="rounded-full border-[4px] border-primary"
+          src={storyImage}
+          alt="story"
+          width={100}
+          height={100}
+          className="w-full h-full group-hover:scale-[1.08] transition-all"
         />
-        <span className="text-base text-white font-semibold">{profileName}</span>
+        <div className="absolute w-full h-full bg-black/20 group-hover:bg-black/50 transition-all inset-0 flex items-start justify-between p-4 flex-col">
+          <Image
+            src={profileImg}
+            alt="user_profile"
+            width={35}
+            height={35}
+            className="rounded-full border-[4px] border-primary"
+          />
+          <span className="text-base text-white font-semibold">
+            {profileName}
+          </span>
+        </div>
       </div>
     </div>
   );
--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -12,31 +12,36 @@ const Homepage = () => {
   return (
     <MainLayout>
       <section className="w-full h-screen overflow-hidden flex items-start justify-start mt-[2px]">
-        <div className="lg:flex-[0.8] w-full hidden md:flex">
+        <div className="lg:flex-[0.8] md:w-[35%] w-full hidden md:flex">
           <LeftSidebar />
         </div>
         <div className="lg:flex-[2] w-full h-screen overflow-y-auto scrollbar-hide p-4 flex flex-col items-start justify-start gap-4">
-          <div className="bg-white py-4 w-full relative px-2">
+          <div className="bg-white py-4 w-full relative px-2 flex items-center">
             <BsChevronLeft
               // onClick={slideLeft}
               size={40}
               className="bg-white rounded-full"
             />
-            <div ref={sliderRef} className="w-full">
-              <div className="relative w-full flex flex-col items-center justify-center rounded-md overflow-hidden cursor-pointer">
-                <div className="relative group overflow-hidden rounded-tr-md rounded-tl-md">
-                  <img
-                    src="/img/avatar.png"
-                    alt="user_profile"
-                    className="w-full h-[200px] mix-blend-darken group-hover:scale-[1.08] transition-all"
-                  />
-                  <div className="absolute w-full h-full group-hover:bg-black/40 transition-all inset-0"></div>
-                </div>
-                <div className="flex flex-col items-center justify-center gap-4 bg-basegray w-full text-white pt-6 pb-2 relative">
-                  <span className="w-10 h-10 bg-primary text-white flex items-center justify-center rounded-full absolute -top-5">
-                    <FiPlus size={20} />
-                  </span>
-                  <span>Create story</span>
+            <div
+              ref={sliderRef}
+              className="w-full h-full overflow-x-scroll overflow-y-hidden whitespace-nowrap scroll-smooth scrollbar-hide relative"
+            >
+              <div className="relative inline-block cursor-pointer w-[200px] h-full mx-1">
+                <div className="relative top-[-57px] w-full h-full flex flex-col items-center justify-center rounded-md cursor-pointer overflow-hidden">
+                  <div className="relative group overflow-hidden rounded-tr-md rounded-tl-md h-full">
+                    <img
+                      src="/img/avatar.png"
+                      alt="user_profile"
+                      className="w-full h-full mix-blend-darken group-hover:scale-[1.08] transition-all"
+                    />
+                    <div className="absolute w-full h-full group-hover:bg-black/40 transition-all inset-0"></div>
+                  </div>
+                  <div className="flex flex-col items-center justify-center gap-4 bg-basegray w-full text-white pt-6 pb-2 relative">
+                    <span className="w-10 h-10 bg-primary text-white flex items-center justify-center rounded-full absolute -top-5">
+                      <FiPlus size={20} />
+                    </span>
+                    <span>Create story</span>
+                  </div>
                 </div>
               </div>
               {cardData.map((item: StoryProp, i: number) => (
@@ -49,6 +54,7 @@ const Homepage = () => {
               className="bg-white rounded-full"
             />
           </div>
+          <div className="w-full"></div>
         </div>
         <div className="lg:flex-[0.8] hidden lg:flex">
           <RightSidebar />
