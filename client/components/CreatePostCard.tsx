e78b380011047cb16bcadea1c3f4ddf7b2baf67e
gift56 <efeasiughu@gmail.com>
2023-09-15 20:07:00 -0400

    working on create story modal

--- client/components/CreatePostCard.tsx
+++ client/components/CreatePostCard.tsx
@@ -6,7 +6,7 @@ import Image from "next/image";
 const CreatePostCard = () => {
   return (
     <div className="w-full bg-white p-3 flex flex-col items-start justify-start">
-      <div className="w-full flex items-center justify-start gap-4">
+      <div className="w-full flex items-center justify-start gap-4 border-b pb-5 border-btngray">
         <Image
           src="/img/avatar.png"
           alt="User_profile"
@@ -14,9 +14,20 @@ const CreatePostCard = () => {
           height={40}
           className="rounded-full"
         />
-        <input type="text" placeholder="What's on your Mind, Efe" className="w-full bg-bodybg h-10 outline-none border border-bodybg focus:border-primary transition-all px-4 text-base font-normal text-basegray placeholder:text-basegray rounded-full" />
+        <input
+          type="text"
+          placeholder="What's on your Mind, Efe"
+          className="w-full bg-bodybg h-10 outline-none border border-bodybg focus:border-primary transition-all px-4 text-base font-normal text-basegray placeholder:text-basegray rounded-full"
+        />
       </div>
-      <div></div>
+      <div className="flex items-center justify-between gap-4 w-full pt-3">
+        <div className="flex items-center gap-4 justify-start w-fit bg-bodybg py-3 px-5 rounded-md cursor-pointer">
+          <span className="text-danger">
+            <RiLiveLine size={25} />
+          </span>
+          <span className="text-base font-normal text-basegray">Live video</span>
+        </div>
+      </div>
     </div>
   );
 };
