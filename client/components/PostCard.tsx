5b90c80bc8e4a6774cc6b29c28e7c159bd906a1a
gift56 <efeasiughu@gmail.com>
2023-09-15 20:32:03 -0400

    working on post cards

--- client/components/PostCard.tsx
+++ client/components/PostCard.tsx
@@ -1,4 +1,16 @@
-const PostCard = () => {
+interface PostCardProp {
+  id: String | Number;
+  userProfileUrl: String;
+  userName: String;
+  date: String | Number;
+  userPost: String;
+}
+
+interface IProp {
+  post: PostCardProp;
+}
+
+const PostCard = ({ post }: IProp) => {
   return <div>PostCard</div>;
 };
 
--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -2,9 +2,11 @@ import MainLayout from "@/layout/MainLayout";
 import {
   CreatePostCard,
   LeftSidebar,
+  PostCard,
   RightSidebar,
   Stories,
 } from "@/components";
+import { postData } from "@/data";
 
 const Homepage = () => {
   return (
@@ -16,6 +18,11 @@ const Homepage = () => {
         <div className="lg:flex-[2] w-full h-screen overflow-y-auto scrollbar-hide p-4 flex flex-col items-start justify-start gap-4">
           <Stories />
           <CreatePostCard />
+          <div className="w-full flex flex-col items-start justify-start gap-6">
+            {postData.map((item) => (
+              <PostCard key={item.id} post={item} />
+            ))}
+          </div>
         </div>
         <div className="lg:flex-[0.8] hidden lg:flex">
           <RightSidebar />
