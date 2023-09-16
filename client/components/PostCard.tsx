60c016dc1e010891c3c0d9cf89dc97af26665348
gift56 <efeasiughu@gmail.com>
2023-09-15 20:34:56 -0400

    working on post cards

--- client/components/PostCard.tsx
+++ client/components/PostCard.tsx
@@ -11,7 +11,13 @@ interface IProp {
 }
 
 const PostCard = ({ post }: IProp) => {
-  return <div className="w-full bg-white p-3 rounded-lg">PostCard</div>;
+  return (
+    <div className="w-full bg-white p-3 rounded-lg flex flex-col items-start justify-start h-fit gap-4">
+      <div className="w-full flex items-center justify-between gap-4">
+        
+      </div>
+    </div>
+  );
 };
 
 export default PostCard;
