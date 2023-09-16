77702f62109305bffda64a0e4fb05dc9ee15b989
gift56 <efeasiughu@gmail.com>
2023-09-15 20:36:15 -0400

    working on post cards

--- client/components/PostCard.tsx
+++ client/components/PostCard.tsx
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 interface PostCardProp {
   id: String | Number;
   userProfileUrl: String;
@@ -14,7 +16,9 @@ const PostCard = ({ post }: IProp) => {
   return (
     <div className="w-full bg-white p-3 rounded-lg flex flex-col items-start justify-start h-fit gap-4">
       <div className="w-full flex items-center justify-between gap-4">
-        
+        <div className="flex items-start justify-start gap-2">
+          <Link href={`/profile/${post.id}`}></Link>
+        </div>
       </div>
     </div>
   );
