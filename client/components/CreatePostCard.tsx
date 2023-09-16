f415c54b5a2ed44ea5d654fbd9f65ae34209908a
gift56 <efeasiughu@gmail.com>
2023-09-15 20:09:24 -0400

    working on create story modal

--- client/components/CreatePostCard.tsx
+++ client/components/CreatePostCard.tsx
@@ -20,12 +20,24 @@ const CreatePostCard = () => {
           className="w-full bg-bodybg h-10 outline-none border border-bodybg focus:border-primary transition-all px-4 text-base font-normal text-basegray placeholder:text-basegray rounded-full"
         />
       </div>
-      <div className="flex items-center justify-between gap-4 w-full pt-3">
-        <div className="flex items-center gap-4 justify-start w-fit bg-bodybg py-3 px-5 rounded-md cursor-pointer">
+      <div className="flex items-center justify-between lg:justify-evenly gap-4 w-full pt-3">
+        <div className="flex items-center gap-4 justify-start w-fit hover:bg-bodybg transition-all py-3 px-5 rounded-md cursor-pointer">
           <span className="text-danger">
             <RiLiveLine size={25} />
           </span>
-          <span className="text-base font-normal text-basegray">Live video</span>
+          <span className="text-base font-medium text-basegray">Live video</span>
+        </div>
+        <div className="flex items-center gap-4 justify-start w-fit hover:bg-bodybg transition-all py-3 px-5 rounded-md cursor-pointer">
+          <span className="text-success">
+            <MdOutlinePhotoLibrary size={25} />
+          </span>
+          <span className="text-base font-medium text-basegray">Photo/video</span>
+        </div>
+        <div className="flex items-center gap-4 justify-start w-fit hover:bg-bodybg transition-all py-3 px-5 rounded-md cursor-pointer">
+          <span className="text-warning">
+            <LiaLaugh size={25} />
+          </span>
+          <span className="text-base font-medium text-basegray">Feeling/activity</span>
         </div>
       </div>
     </div>
