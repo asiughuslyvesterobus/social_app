31f78288f983b41c5b589b50887fdaf2c7869746
gift56 <efeasiughu@gmail.com>
2023-09-21 19:10:17 -0400

    working on authentication

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -1,12 +1,13 @@
+import { AuthProvider } from "@/context/AuthContext";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { Toaster } from "react-hot-toast";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <AuthProvider>
       <Component {...pageProps} />
       <Toaster />
-    </>
+    </AuthProvider>
   );
 }
