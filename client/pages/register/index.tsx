4f7ca804cf6f5e3688886a6e2db7f466ffa681e5
gift56 <efeasiughu@gmail.com>
2023-09-21 15:34:14 -0400

    working on registration page

--- client/pages/register/index.tsx
+++ client/pages/register/index.tsx
@@ -1,4 +1,19 @@
+import { useState } from "react";
+import { CustomizeInput, CustomizeTextarea } from "@/components";
+import Head from "next/head";
+import Link from "next/link";
+import { useFormik } from "formik";
+import { registerSchema } from "@/schema";
+import toast from "react-hot-toast";
+import { SignUpUser } from "@/type";
+import { useRouter } from "next/router";
+import { AiOutlineEye, AiOutlineEyeInvisible } from "react-icons/ai";
+import Image from "next/image";
+
 const Registerpage = () => {
+  const router = useRouter();
+  const [showPassword, setShowPassword] = useState(false);
+  const [showConfirmPassword, setShowConfirmPassword] = useState(false);
   return <div>Registerpage</div>;
 };
 
