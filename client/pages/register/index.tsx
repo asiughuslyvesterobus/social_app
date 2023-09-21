0306488e497ffa78b1d79e9ff707f41db3fb1339
gift56 <efeasiughu@gmail.com>
2023-09-21 15:36:17 -0400

    working on registration page

--- client/pages/register/index.tsx
+++ client/pages/register/index.tsx
@@ -14,6 +14,41 @@ const Registerpage = () => {
   const router = useRouter();
   const [showPassword, setShowPassword] = useState(false);
   const [showConfirmPassword, setShowConfirmPassword] = useState(false);
+
+  const onSubmit = async (payload: SignUpUser, actions: any) => {
+    console.log(payload);
+    router.push("/");
+    toast.success("Registrated Successfully 😊!", {
+      duration: 2000,
+    });
+    await new Promise((res) => setTimeout(res, 1000));
+    actions.resetForm();
+  };
+
+  const {
+    values,
+    handleChange,
+    handleBlur,
+    handleSubmit,
+    touched,
+    errors,
+    isSubmitting,
+  } = useFormik({
+    initialValues,
+    validationSchema: registerSchema,
+    onSubmit,
+  });
+
+  const getError = (key: keyof SignUpUser) => {
+    return touched[key] && errors[key];
+  };
+
+  const toggle = () => {
+    setShowPassword((prev) => !prev);
+  };
+  const toggleConfirm = () => {
+    setShowConfirmPassword((prev) => !prev);
+  };
   return <div>Registerpage</div>;
 };
 
