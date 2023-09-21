c16c07cf5cb83263d778ef82b6202fec914f7862
gift56 <efeasiughu@gmail.com>
2023-09-21 14:19:24 -0400

    working on the other pages

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,7 +1,18 @@
+import { useState } from "react";
+import { CustomizeInput } from "@/components";
+import Head from "next/head";
+import Link from "next/link";
 import { useFormik } from "formik";
-import { SignUpUser } from "@/type";
+import { loginSchema } from "@/schema";
+import { toast } from "react-hot-toast";
 
 const Loginpage = () => {
+  const [showPassword, setShowPassword] = useState(false);
+
+  const toggle = () => {
+    setShowPassword((prev) => !prev);
+  };
+
   return (
     <div className="w-full flex flex-col h-screen items-center justify-center lg:flex-row lg:overflow-hidden">
       <div></div>
