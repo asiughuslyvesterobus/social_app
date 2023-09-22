c16d0525e267f2dc7b4518197ff289a4ce43e001
gift56 <efeasiughu@gmail.com>
2023-09-22 14:23:31 -0400

    working on mobile nav

--- client/components/mobileNav/MobileNav.tsx
+++ client/components/mobileNav/MobileNav.tsx
@@ -1,6 +1,9 @@
 import { useRouter } from "next/router";
 import { useRef, useEffect } from "react";
+import Image from "next/image";
 import Link from "next/link";
+import { NavIcons, sideBarLink } from "@/data";
+import { NavIconType } from "@/type";
 
 interface MenuProps {
   show: boolean;
@@ -35,8 +38,44 @@ const MobileNav = ({ show, setShow }: MenuProps) => {
 
   return (
     <div
-      className={`md:hidden absolute top-[100%] border-t z-50 bg-white shadow-boxShad w-full left-0 h-screen overflow-y-auto p-4 flex-col gap-4 items-start justify-start`}
-    ></div>
+      className={`md:hidden absolute top-[100%] border-t z-50 bg-white shadow-boxShad w-full left-0 h-screen overflow-y-auto p-4 flex-col items-start justify-start`}
+    >
+      {NavIcons.map((item: NavIconType, i: number) => (
+        <li
+          key={i}
+          className={`py-4 flex items-center justify-start gap-4 cursor-pointer ${
+            activeRoute(item.href)
+              ? "text-primary border-b border-primary"
+              : "hover:bg-bodybg mb-1 rounded-lg"
+          } transition-all`}
+        >
+          <Link onClick={() => setShow(false)} href={item.href}>
+            <span>
+              <item.icon size={20} />
+            </span>
+          </Link>
+          <span className="font-semibold text-sm">{item.tooltip}</span>
+        </li>
+      ))}
+      <ul>
+        {sideBarLink.map((item, i) => (
+          <li key={i} className="w-full">
+            <Link
+              href={item.href}
+              className="flex items-center justify-start w-full gap-4 py-4 transition-all"
+            >
+              <Image
+                src={item.imgUrl}
+                alt={`${item.text}_icon`}
+                width={18}
+                height={20}
+              />
+              <span className="text-sm font-semibold">{item.text}</span>
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </div>
   );
 };
 
