30a9ddb2b16d2ad9bfccc9eab5d689b5600a2943
gift56 <efeasiughu@gmail.com>
2023-09-22 20:03:49 -0400

    working on notification box

--- client/components/notificationBox/NotificationBox.tsx
+++ client/components/notificationBox/NotificationBox.tsx
@@ -1,6 +1,32 @@
-import { useRef, useEffect } from "react";
+import { useRef, useEffect, useState } from "react";
+import Link from "next/link";
+import { BsThreeDots } from "react-icons/bs";
+import { useAuth } from "@/context/AuthContext";
 
-const NotificationBox = () => {
+interface NotificationProps {
+  show: boolean;
+  setShow: (e: any) => void;
+}
+
+const NotificationBox = ({ show, setShow }: NotificationProps) => {
+  const modalRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const handleClickOutside: EventListener = (event) => {
+      if (
+        modalRef.current &&
+        !modalRef.current.contains(event.target as Node)
+      ) {
+        setShow(false);
+      }
+    };
+    document.addEventListener("mousedown", handleClickOutside);
+
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [show]);
+  
   return <div>NotificationBox</div>;
 };
 
