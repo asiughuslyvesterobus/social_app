cbdb77aea5c8655d37f896dca75f31ee93175a4e
gift56 <efeasiughu@gmail.com>
2023-09-22 20:08:15 -0400

    working on notification box

--- client/components/notificationBox/NotificationBox.tsx
+++ client/components/notificationBox/NotificationBox.tsx
@@ -28,7 +28,7 @@ const NotificationBox = ({ show, setShow }: NotificationProps) => {
   }, [show]);
 
   return (
-    <div className="absolute w-[300px] flex flex-col items-start justify-start gap-2 bg-bodybg p-5 z-40 top-[140%] shadow-boxShad right-0 rounded-md">
+    <div className="absolute w-[300px] tab:w-[450px] flex flex-col items-start justify-start gap-2 bg-bodybg p-5 z-40 top-[140%] shadow-boxShad right-[-150%] rounded-md">
       NotificationBox
     </div>
   );
