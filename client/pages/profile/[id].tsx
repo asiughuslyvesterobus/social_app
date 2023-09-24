ec8ea4ec3034ad83e7aead95e6d220ad9d68c907
gift56 <efeasiughu@gmail.com>
2023-09-24 08:16:47 -0400

    working on profile page

--- client/pages/profile/[id].tsx
+++ client/pages/profile/[id].tsx
@@ -8,7 +8,7 @@ const Profilepage = () => {
           <div className="tab:w-[75%] w-full tab:mx-auto flex flex-col items-start justify-start">
             <div className="w-full relative h-[120px] tab:h-[240px] bg-primary tab:rounded-bl-md tab:rounded-br-md"></div>
             <div className="w-full tab:w-[90%] tab:mx-auto tab:px-4 flex flex-col items-center justify-start tab:justify-between tab:flex-row border-b border-basegray">
-              <div className="flex w-full tab:w-fit items-center justify-start flex-col tab:flex-row tab:items-start tab:gap-6 relative">
+              <div className="flex w-full items-center justify-start flex-col tab:flex-row tab:items-start tab:gap-6 relative">
                 <div className="absolute tab:relative bg-btngray rounded-full p-1 -translate-y-14">
                   <img
                     src="/img/avatar.png"
@@ -16,11 +16,16 @@ const Profilepage = () => {
                     className="w-40 h-40 rounded-full border-[4px] border-primary"
                   />
                 </div>
-                <div className="flex flex-col items-center tab:items-start justify-start tab:pt-5 mt-32 tab:mt-0">
+                <div className="flex flex-col items-center tab:items-start flex-grow justify-start tab:pt-5 mt-32 tab:mt-0">
                   <h2 className="text-3xl font-bold tab:text-4xl">
                     Efe Starboy
                   </h2>
-                  <p className="text-base font-medium text-gray-600">249 friends</p>
+                  <div className="flex items-center justify-center tab:justify-between flex-col tab:flex-row gap-4 w-full">
+                    <p className="text-base font-medium text-gray-600">
+                      249 friends
+                    </p>
+                    <p>djdjd</p>
+                  </div>
                 </div>
               </div>
               <div></div>
