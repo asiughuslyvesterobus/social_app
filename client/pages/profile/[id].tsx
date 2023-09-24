601173926347c7c1081efa0f8613db05d78031ff
gift56 <efeasiughu@gmail.com>
2023-09-23 20:52:55 -0400

    working on profile page

--- client/pages/profile/[id].tsx
+++ client/pages/profile/[id].tsx
@@ -4,8 +4,11 @@ const Profilepage = () => {
   return (
     <MainLayout title="User Profile">
       <section className="w-full flex-col items-start justify-start gap-5">
-        <div className="w-full bg-btngray tab:px-5 tab:pt-5 shadow-boxShad flex flex-col items-center justify-center">
-          <div className="tab:w-[60%] tab:mx-auto flex flex-col items-start justify-start gap-4"></div>
+        <div className="w-full bg-btngray tab:px-5 shadow-boxShad flex gap-5 flex-col items-center justify-center">
+          <div className="tab:w-[75%] w-full tab:mx-auto flex flex-col items-start justify-start gap-4">
+            <div className="w-full relative h-[120px] tab:h-[240px] bg-primary tab:rounded-bl-md tab:rounded-br-md"></div>
+            <div className="w-full tab:w-[60%] tab:mx-auto flex flex-col items-center justify-start tab:justify-between"></div>
+          </div>
         </div>
       </section>
     </MainLayout>
