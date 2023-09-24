fd06633e2b12f31e6c384a0bf7cd90ec2201daab
gift56 <efeasiughu@gmail.com>
2023-09-23 20:43:04 -0400

    working on profile page

--- client/pages/profile/[id].tsx
+++ client/pages/profile/[id].tsx
@@ -1,7 +1,9 @@
 import MainLayout from "@/layout/MainLayout";
 
 const Profilepage = () => {
-  return <MainLayout title="User Profil">Profilepage</MainLayout>;
+  return <MainLayout title="User Profile">
+    <section className="w-full flex-col items-start justify-start gap-5"></section>
+  </MainLayout>;
 };
 
 export default Profilepage;
