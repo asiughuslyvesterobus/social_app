094fde32020983ad6ab55de2828f60127ad9ad60
gift56 <efeasiughu@gmail.com>
2023-09-23 20:55:37 -0400

    working on profile page

--- client/pages/profile/[id].tsx
+++ client/pages/profile/[id].tsx
@@ -7,7 +7,7 @@ const Profilepage = () => {
         <div className="w-full bg-btngray tab:px-5 shadow-boxShad flex gap-5 flex-col items-center justify-center">
           <div className="tab:w-[75%] w-full tab:mx-auto flex flex-col items-start justify-start gap-4">
             <div className="w-full relative h-[120px] tab:h-[240px] bg-primary tab:rounded-bl-md tab:rounded-br-md"></div>
-            <div className="w-full px-4 flex flex-col items-center justify-start tab:justify-between tab:flex-row border-b border-basegray pb-5">
+            <div className="w-full tab:w-[90%] tab:mx-auto tab:px-4 flex flex-col items-center justify-start tab:justify-between tab:flex-row border-b border-basegray pb-5">
 
             </div>
           </div>
