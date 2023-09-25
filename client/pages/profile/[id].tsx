9f5607d50673c91a0c094766167c4b649bed9660
gift56 <efeasiughu@gmail.com>
2023-09-25 11:57:36 -0400

    working on profile page

--- client/pages/profile/[id].tsx
+++ client/pages/profile/[id].tsx
@@ -80,7 +80,7 @@ const Profilepage = () => {
                 </Link>
               </div>
               <div className="w-full grid grid-cols-3 gap-3 items-start justify-start">
-                {[0, 1, 2.3, 4, 5, 6, 8, 9,10].map((item, i) => (
+                {[0, 1, 2.3, 4, 5, 6, 8, 9, 10].map((item, i) => (
                   <div className="w-full flex flex-col items-center justify-center gap-1">
                     <div className="w-full h-20 md:h-[120px] cursor-pointer">
                       <img
