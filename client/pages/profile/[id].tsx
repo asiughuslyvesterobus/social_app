77c23ca9a0b490f42d2d9329cc8a41cd04569001
gift56 <efeasiughu@gmail.com>
2023-09-25 12:02:33 -0400

    done working on profile page

--- client/pages/profile/[id].tsx
+++ client/pages/profile/[id].tsx
@@ -64,7 +64,7 @@ const Profilepage = () => {
           </div>
         </div>
         <div className="tab:w-[75%] w-full tab:mx-auto flex flex-col items-start justify-start tab:flex-row py-10 gap-8 px-4 tab:px-0">
-          <div className="w-full flex flex-col items-start justify-start gap-5 sticky top-10">
+          <div className="w-full tab:flex-[1] flex flex-col items-start justify-start gap-5 sticky top-0">
             <div className="w-full bg-bodybg border shadow-boxShad rounded-lg p-3 flex flex-col items-start justify-start gap-5">
               <h2 className="text-lg font-bold md:text-2xl">Intro</h2>
               <div className="w-full flex flex-col items-center justify-center">
@@ -84,7 +84,7 @@ const Profilepage = () => {
               <div className="w-full grid grid-cols-3 gap-3 items-start justify-start">
                 {[0, 1, 2.3, 4, 5, 6, 8, 9, 10].map((item, i) => (
                   <div className="w-full flex flex-col items-center justify-center gap-1">
-                    <div className="w-full h-20 md:h-[120px] cursor-pointer">
+                    <div className="w-full h-20 md:h-[100px] cursor-pointer">
                       <img
                         src="https://images.pexels.com/photos/18141660/pexels-photo-18141660/free-photo-of-a-woman-sitting-on-the-steps-with-her-dog.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
                         alt="friends_image"
@@ -99,7 +99,7 @@ const Profilepage = () => {
               </div>
             </div>
           </div>
-          <div className="w-full h-screen overflow-y-auto overflow-x-hidden scrollbar-hide p-3 flex flex-col items-start justify-start gap-5">
+          <div className="w-full tab:flex-[1.3] h-full overflow-y-auto overflow-x-hidden scrollbar-hide p-3 flex flex-col items-start justify-start gap-5">
             <div className="w-full flex flex-col items-start justify-start gap-6">
               {postData.map((item) => (
                 <PostCard key={item.id} post={item} />
