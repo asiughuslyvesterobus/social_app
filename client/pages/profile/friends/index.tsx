2f3d348cf7cd33e8336f50b2a880e010f18960a2
gift56 <efeasiughu@gmail.com>
2023-09-30 11:37:33 -0400

    working on friends page

--- client/pages/profile/friends/index.tsx
+++ client/pages/profile/friends/index.tsx
@@ -1,5 +1,17 @@
+import { ProfileHeadline } from "@/components";
+import MainLayout from "@/layout/MainLayout";
+import Link from "next/link";
+
 const FriendsPage = () => {
-  return <div>FriendsPage</div>;
+  return (
+    <MainLayout title="Friends">
+      <section className="w-full flex-col items-start justify-start gap-5 h-screen overflow-y-auto overflow-x-hidden pb-20">
+        <div className="w-full bg-btngray tab:px-5 shadow-boxShad flex gap-5 flex-col items-center justify-center">
+          <ProfileHeadline className="hover:bg-gray-300 rounded-lg" />
+        </div>
+      </section>
+    </MainLayout>
+  );
 };
 
 export default FriendsPage;
