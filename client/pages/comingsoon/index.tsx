06f521a8929ec3c45128b3c47554e38098da113d
gift56 <efeasiughu@gmail.com>
2023-10-01 19:24:21 -0400

    working on notification

--- client/pages/comingsoon/index.tsx
+++ client/pages/comingsoon/index.tsx
@@ -1,5 +1,16 @@
+import MainLayout from "@/layout/MainLayout";
+
 const ComingSoonPage = () => {
-  return <div>ComingSoonPage</div>;
+  return (
+    <MainLayout title="Coming soon">
+      <div className="w-full flex flex-col items-center justify-center gap-4">
+        <p className="text-center text-sm font-medium md:text-base">
+          Get ready to embark on a journey like never before. We're cooking up
+          something extraordinary just for YOU! 🌐✨
+        </p>
+      </div>
+    </MainLayout>
+  );
 };
 
 export default ComingSoonPage;
