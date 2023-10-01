af7f02640f1c548b7e609bfe86c5c935ef3e7c81
gift56 <efeasiughu@gmail.com>
2023-10-01 18:33:37 -0400

    working on notification

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -2,8 +2,17 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { Toaster } from "react-hot-toast";
 import { AuthProvider } from "@/context/AuthContext";
+import { useState, useEffect } from "react";
 
 export default function App({ Component, pageProps }: AppProps) {
+  const [isSSR, setIsSSR] = useState(true);
+
+  useEffect(() => {
+    setIsSSR(false);
+  }, []);
+
+  if (isSSR) return null;
+
   return (
     <AuthProvider>
       <Component {...pageProps} />
