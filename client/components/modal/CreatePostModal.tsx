ac0d2cf2f058e0483f1cd8da97b17c3b5a8015c3
gift56 <efeasiughu@gmail.com>
2023-10-02 14:57:13 -0400

    working on notification

--- client/components/modal/CreatePostModal.tsx
+++ client/components/modal/CreatePostModal.tsx
@@ -1,5 +1,7 @@
 import { MdClose } from "react-icons/md";
 import { useState, useEffect, useRef } from "react";
+import Link from "next/link";
+import Image from "next/image";
 
 interface ModalProp {
   show: boolean;
@@ -38,7 +40,7 @@ const CreatePostModal = ({ show, setShow }: ModalProp) => {
       <div
         className={`${
           show ? "scale-100 opacity-100" : "scale-100 opacity-100"
-        } lg:w-[450px] w-full bg-white rounded-lg flex flex-col items-start justify-start gap-3 transition-all duration-300 h-[450px] overflow-y-auto p-4 lg:px-4`}
+        } lg:w-[450px] w-[90%] bg-white rounded-lg flex flex-col items-start justify-start gap-3 transition-all duration-300 p-4 lg:px-4`}
       >
         <div className="w-full flex items-center justify-between gap-4 border-b pb-2">
           <h3 className="text-2xl font-bold">Create Post</h3>
@@ -51,7 +53,37 @@ const CreatePostModal = ({ show, setShow }: ModalProp) => {
             <MdClose className="hover:rotate-180 transition-all duration-300 w-5 h-5" />
           </span>
         </div>
-        <div className="w-full flex flex-col"></div>
+        <div className="w-full flex flex-col items-start justify-start gap-6">
+          <Link
+            href={`/profile/42`}
+            className="flex items-center justify-start w-full gap-2 hover:bg-bodybg py-2 px-2 rounded transition-all"
+          >
+            <Image
+              src="/img/avatar.png"
+              alt="user_profile"
+              width={30}
+              height={30}
+              className="rounded-full"
+            />
+            <span className="text-base font-semibold text-black tracking-tight">
+              Efe Starboy
+            </span>
+          </Link>
+          <div className="w-full flex flex-col">
+            <textarea
+              name="content"
+              rows={4}
+              placeholder="What's on your mind Efe"
+              className="outline-none border-none h-[200px] w-full text-base md:text-2xl font-medium text-basegray resize-none"
+            ></textarea>
+            <button
+              disabled={true}
+              className="text-base font-semibold bg-primary text-white transition-all duration-300 disabled:cursor-not-allowed disabled:bg-basegray/40 h-10 outline-none rounded-md"
+            >
+              Post
+            </button>
+          </div>
+        </div>
       </div>
     </div>
   );
