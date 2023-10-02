92c6b15c3b947e3b30ab4bb4d546926120fe54d1
gift56 <efeasiughu@gmail.com>
2023-10-02 07:11:38 -0400

    working on notification

--- client/components/modal/CreatePostModal.tsx
+++ client/components/modal/CreatePostModal.tsx
@@ -28,8 +28,16 @@ const CreatePostModal = ({ show, setShow }: ModalProp) => {
       document.removeEventListener("mousedown", handleClickOutside);
     };
   }, [show]);
-  
-  return <div>CreatePostModal</div>;
+
+  return (
+    <div
+      className={`fixed top-0 right-0 w-full h-full bg-[#00000085] z-40 place-items-center flex justify-center transition-all duration-500 overflow-auto ${
+        show ? "flex" : "flex"
+      }`}
+    >
+
+    </div>
+  );
 };
 
 export default CreatePostModal;
