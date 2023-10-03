9669936728c1a2c6205006546bbe970ff257ef64
gift56 <efeasiughu@gmail.com>
2023-10-03 11:35:43 -0400

    working on some updates

--- client/pages/groups/feed/index.tsx
+++ client/pages/groups/feed/index.tsx
@@ -1,7 +1,18 @@
 import MainLayout from "@/layout/MainLayout";
 
 const GroupPage = () => {
-  return <MainLayout title="Groups">GroupPage</MainLayout>;
+  return (
+    <MainLayout title="Groups">
+      <section className="w-full h-screen overflow-y-auto overflow-x-hidden flex items-start justify-start mt-[2px]">
+        <div className="lg:flex-shrink bg-white md:w-[35%] w-full hidden md:flex flex-col">
+
+        </div>
+        <div className="lg:flex-grow w-full h-screen overflow-y-auto scrollbar-hide p-4 flex flex-col items-start justify-start gap-4 pb-24">
+          hdhde
+        </div>
+      </section>
+    </MainLayout>
+  );
 };
 
 export default GroupPage;
