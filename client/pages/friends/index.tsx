264778e73fa86c9407d4b442926383175acfd14e
gift56 <efeasiughu@gmail.com>
2023-10-05 15:01:52 -0400

    working on friends page

--- client/pages/friends/index.tsx
+++ client/pages/friends/index.tsx
@@ -1,7 +1,13 @@
 import MainLayout from "@/layout/MainLayout";
 
 const FriendsPage = () => {
-  return <MainLayout title="Friends">FriendsPage</MainLayout>;
+  return (
+    <MainLayout title="Friends">
+      <section className="w-full h-screen overflow-auto flex flex-col tab:flex-row items-start justify-start mt-[2px]">
+        
+      </section>
+    </MainLayout>
+  );
 };
 
 export default FriendsPage;
