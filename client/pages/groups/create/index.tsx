41c4c4f5978d0549934cd4960973aa00bdef54ff
gift56 <efeasiughu@gmail.com>
2023-10-05 05:12:44 -0400

    working on create group page

--- client/pages/groups/create/index.tsx
+++ client/pages/groups/create/index.tsx
@@ -1,4 +1,5 @@
 import MainLayout from "@/layout/MainLayout";
+import Image from "next/image";
 import Link from "next/link";
 import { FiChevronRight } from "react-icons/fi";
 
@@ -14,7 +15,22 @@ const Createroup = () => {
             <FiChevronRight />
             <span>Create Group</span>
           </p>
-          <h2 className="text-lg font-bold md:text-2xl">Create Gruop</h2>
+          <h2 className="text-lg font-bold md:text-2xl">Create Group</h2>
+          <div className="flex items-start justify-start gap-2">
+            <Image
+              src=""
+              alt="Profile-image"
+              width={40}
+              height={40}
+              className="rounded-full"
+            />
+            <div className="flex flex-col items-start justify-start">
+              <h2 className="text-base font-medium text-black">Efe Starboy</h2>
+              <span className="text-sm font-normal text-[#888] flex items-center gap-1">
+                Admin
+              </span>
+            </div>
+          </div>
         </div>
         <div className="tab:flex-grow relative w-full tab:h-screen tab:overflow-y-auto scrollbar-hide p-4 flex flex-col items-start justify-start gap-6 pb-24"></div>
       </section>
