8c43a829620d91dddc6798d73b042ff6aee0e6d6
gift56 <efeasiughu@gmail.com>
2023-10-07 13:15:51 -0400

    done working on some authentication

--- client/pages/notification/index.tsx
+++ client/pages/notification/index.tsx
@@ -2,6 +2,7 @@ import { useState, useEffect, useRef } from "react";
 import MainLayout from "@/layout/MainLayout";
 import Link from "next/link";
 import { FaUserSecret } from "react-icons/fa";
+import { ProtectedRoute } from "@/components";
 
 const NotificationPage = () => {
   const [data, setData] = useState([
@@ -42,19 +43,20 @@ const NotificationPage = () => {
 
   return (
     <MainLayout title="Notifcation">
-      <div className="w-full flex items-center justify-center mt-5">
-        <div
-          ref={notificationContainerRef}
-          className="w-full md:w-[500px] bg-white h-[90vh] rounded-md flex flex-col overflow-y-auto scrollbar-hide overflow-x-hidden gap-6 p-4"
-        >
-          <div className="w-full flex items-center justify-between gap-2">
-            <h2 className="text-xl font-bold md:text-2xl">Notifications</h2>
-            <span className="text-sm md:text-base font-semibold cursor-pointer">
-              Read All
-            </span>
-          </div>
-          <div className="w-full flex items-start justify-start flex-col">
-            {/* <div className="animate-pulse flex space-x-4 w-full">
+      <ProtectedRoute>
+        <div className="w-full flex items-center justify-center mt-5">
+          <div
+            ref={notificationContainerRef}
+            className="w-full md:w-[500px] bg-white h-[90vh] rounded-md flex flex-col overflow-y-auto scrollbar-hide overflow-x-hidden gap-6 p-4"
+          >
+            <div className="w-full flex items-center justify-between gap-2">
+              <h2 className="text-xl font-bold md:text-2xl">Notifications</h2>
+              <span className="text-sm md:text-base font-semibold cursor-pointer">
+                Read All
+              </span>
+            </div>
+            <div className="w-full flex items-start justify-start flex-col">
+              {/* <div className="animate-pulse flex space-x-4 w-full">
               <div className="rounded-full bg-slate-400 h-10 w-10"></div>
               <div className="flex-1 space-y-2 py-1">
                 <div className="h-2 bg-slate-400 rounded"></div>
@@ -62,25 +64,26 @@ const NotificationPage = () => {
                 <div className="w-[50%] h-2 bg-slate-400 rounded"></div>
               </div>
             </div> */}
-            {data.map((item, i) => (
-              <Link
-                key={i}
-                href="/notification"
-                className="relative w-full flex items-start justify-start gap-3 hover:bg-btngray py-2 px-3 rounded-lg transition-all duration-300"
-              >
-                <FaUserSecret size={30} />
-                <div className="w-full flex flex-col items-start justify-start gap-3">
-                  <p className="text-sm font-medium md:text-base">
-                    <b>Hey Efe Starboy</b>, You have a new message from Bruce
-                    wayne!
-                  </p>
-                </div>
-              </Link>
-            ))}
-            {loading && <p>Loading...</p>}
+              {data.map((item, i) => (
+                <Link
+                  key={i}
+                  href="/notification"
+                  className="relative w-full flex items-start justify-start gap-3 hover:bg-btngray py-2 px-3 rounded-lg transition-all duration-300"
+                >
+                  <FaUserSecret size={30} />
+                  <div className="w-full flex flex-col items-start justify-start gap-3">
+                    <p className="text-sm font-medium md:text-base">
+                      <b>Hey Efe Starboy</b>, You have a new message from Bruce
+                      wayne!
+                    </p>
+                  </div>
+                </Link>
+              ))}
+              {loading && <p>Loading...</p>}
+            </div>
           </div>
         </div>
-      </div>
+      </ProtectedRoute>
     </MainLayout>
   );
 };
